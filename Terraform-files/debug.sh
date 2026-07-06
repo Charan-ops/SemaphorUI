f66ab87c20f2271d7ad95688109532882212f7a6
Charan <charan.murthy@sifycorp.com>
2026-07-07 00:19:27 +0530

    Testing debug.sh

--- Terraform-files/debug.sh
+++ Terraform-files/debug.sh
@@ -1,14 +1,25 @@
-echo "===== DEBUG ====="
-whoami
-echo "HOME=$HOME"
-pwd
+# echo "===== DEBUG ====="
+# whoami
+# echo "HOME=$HOME"
+# pwd
 
-ls -la $HOME
-ls -la $HOME/.aws || true
+# ls -la $HOME
+# ls -la $HOME/.aws || true
 
-cat $HOME/.aws/config || true
-cat $HOME/.aws/credentials || true
+# cat $HOME/.aws/config || true
+# cat $HOME/.aws/credentials || true
 
-env | grep AWS || true
+# env | grep AWS || true
 
-aws configure list || true
+# aws configure list || true
+
+
+#!/bin/bash
+
+cd Terraform-files
+
+terraform state list
+
+echo "----------------"
+
+terraform show
